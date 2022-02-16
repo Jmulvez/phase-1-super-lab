dd8aefc59bedaf2a4ebe20bbb48d80c39fad27dc
John <johnmulvey48@yahoo.com>
2022-02-16 11:03:16 -0500

    git@github.com:Jmulvez/phase-1-super-lab.git

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your classes here
+class tree {
+    constructor(species) {
+        this.species = species;
+    }
+}
+definition() {
+    return 'A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.'
+}
+
+class deciduous {
+    constructor(species, name) {
+        this.species = species;
+        super(name);
+    }
+    definition() {
+        return 'Deciduous trees shed their leaves annually.'
+    }
+}
+
+class evergreen {
+    constructor(species, name) {
+        super(species);
+        this.name = name;
+    }
+    definition() {
+        return 'Evergreens keep their leaves all year round.'
+    }
+}
